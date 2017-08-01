77ab734573912afe59b12ffde76f94698a69e8be
Javi Martin <webmasterhhp@gmail.com>
2017-08-01 17:52:09 +0200

    Extends from Activity and can add elements to listview when loaded from URL

--- app/src/main/java/yt/javi/quotes/quotesapp/MainActivity.kt
+++ app/src/main/java/yt/javi/quotes/quotesapp/MainActivity.kt
@@ -1,19 +1,20 @@
 package yt.javi.quotes.quotesapp
 
-import android.app.ListActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
 
 
-class MainActivity : ListActivity() {
-    private lateinit var adapter: ArrayAdapter<String>
-
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter= ArrayAdapter(this, R.id.list_item)
-        listAdapter = adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, mutableListOf<String>())
+
+        (findViewById<View>(R.id.list) as ListView).adapter = adapter
         QuotesProvider().getQuotes(adapter)
     }
 }
