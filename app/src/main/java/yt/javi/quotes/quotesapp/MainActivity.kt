1baf931798c1c038eed28ca5ba701b244a9502ac
Javi Martin <webmasterhhp@gmail.com>
2017-08-27 11:39:37 +0200

    Extends from AppCompatActivity

--- app/src/main/java/yt/javi/quotes/quotesapp/MainActivity.kt
+++ app/src/main/java/yt/javi/quotes/quotesapp/MainActivity.kt
@@ -1,20 +1,19 @@
 package yt.javi.quotes.quotesapp
 
-import android.app.Activity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, mutableListOf<String>())
 
-        (findViewById<View>(R.id.list) as ListView).adapter = adapter
+        (findViewById(R.id.list) as ListView).adapter = adapter
         QuotesProvider().getQuotes(adapter)
     }
 }
