d4f24be6d66951496e9da829f3cd6f9064924916
Javi Martín <javiyt@users.noreply.github.com>
2017-08-30 17:21:14 +0200

    Extends from Activity and can add elements to listview when loaded fr… (#1)
    * Extends from Activity and can add elements to listview when loaded from URL
    
    * Extends from AppCompatActivity

--- app/src/main/java/yt/javi/quotes/quotesapp/MainActivity.kt
+++ app/src/main/java/yt/javi/quotes/quotesapp/MainActivity.kt
@@ -1,19 +1,19 @@
 package yt.javi.quotes.quotesapp
 
-import android.app.ListActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
+import android.widget.ListView
 
 
-class MainActivity : ListActivity() {
-    private lateinit var adapter: ArrayAdapter<String>
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter= ArrayAdapter(this, R.id.list_item)
-        listAdapter = adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, mutableListOf<String>())
+
+        (findViewById(R.id.list) as ListView).adapter = adapter
         QuotesProvider().getQuotes(adapter)
     }
 }
--- app/src/main/java/yt/javi/quotes/quotesapp/QuotesProvider.kt
+++ app/src/main/java/yt/javi/quotes/quotesapp/QuotesProvider.kt
@@ -1,5 +1,6 @@
 package yt.javi.quotes.quotesapp
 
+import android.text.Html
 import android.widget.ArrayAdapter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -8,15 +9,26 @@ import org.jetbrains.anko.uiThread
 import java.net.URL
 
 
+
+
 class QuotesProvider {
     inline fun <reified T> Gson.fromJson(json: String) = this.fromJson<T>(json, object : TypeToken<T>() {}.type)
 
-    fun getQuotes(quotes: ArrayAdapter<String>) {
+    fun getQuotes(adapter: ArrayAdapter<String>) {
         doAsync {
             val result = Gson().fromJson<List<Quote>>(URL("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=10").readText())
             uiThread {
-                result.forEach { quotes.add(it.content) }
+                result.forEach { adapter.add(stripHtml(it.content)) }
+                adapter.notifyDataSetChanged()
             }
+        }
+    }
+
+    private fun stripHtml(html: String): String {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            return Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY).toString()
+        } else {
+            return Html.fromHtml(html).toString()
         }
     }
 }
